8c954e914356021a17d7b6d1fb1447368c5c79b0
anandlearns007-ar <anandlearns007@gmail.com>
2026-06-10 00:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 103.7 MB (32.68%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        
+        long long max = *max_element(nums.begin(), nums.end());
+        long long min = *min_element(nums.begin(), nums.end());
+
+        return k * (max - min);
+    }
+};
