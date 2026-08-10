556aa07f418dbda84d27d9160f7204738f3e5934
anandlearns007-ar <anandlearns007@gmail.com>
2026-08-10 13:04:00 +0530

    Time: 7 ms (71.11%) | Memory: 44.9 MB (12.42%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int contentChildren = 0, cookieIndex = 0;
+        while (cookieIndex < s.size() && contentChildren < g.size()) {
+            if(s[cookieIndex] >= g[contentChildren])
+            {
+                  contentChildren++;
+            }
+            cookieIndex++;
+        }
+        return contentChildren;
+    }
+};
